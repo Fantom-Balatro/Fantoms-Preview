b648b7c2898206c28391e8c849b1284bf9b894c8
Fantom-Balatro <fantombalatro@gmail.com>
2025-03-23 19:47:58 -0600

    Update Card.lua

--- FNPreview/Card.lua
+++ FNPreview/Card.lua
@@ -12,3 +12,14 @@ function get_X_same(num, hand)
 	end
 	return orig_get_X_same(num, clean_hand)
 end
+
+local orig_get_highest = get_highest
+function get_highest(hand)
+    local clean_hand = {}
+    for _, v in pairs(hand) do
+        if v.get_nominal then
+            table.insert(clean_hand, v)
+        end
+    end
+    return orig_get_highest(clean_hand)
+end
